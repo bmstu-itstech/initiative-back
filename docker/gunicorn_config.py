657b6b4473b07f88e32ddda23762c5bfe8186c41
Cunev <cunev2005@gmail.com>
2026-07-09 02:10:26 +0300

    JSON logs for gunicorn

--- docker/gunicorn_config.py
+++ docker/gunicorn_config.py
@@ -4,6 +4,8 @@
 
 import multiprocessing
 
+import structlog
+
 bind = '0.0.0.0:8000'
 # Concerning `workers` setting see:
 # https://github.com/wemake-services/wemake-django-template/issues/1022
@@ -12,12 +14,60 @@ workers = min(multiprocessing.cpu_count() * 2 + 1, 8)
 max_requests = 2000
 max_requests_jitter = 400
 
+chdir = '/code'
+worker_tmp_dir = '/dev/shm'  # noqa: S108
+
 accesslog = '-'
 access_log_format = (
-    '{"time": "%(t)s", "remote_ip": "%(h)s", "method": "%(m)s", '
-    '"path": "%(U)s", "status": %(s)s, "time_taken": %(L)s, '
+    '{"time": "%(t)s", "level": "INFO", "event": "http_request", '
+    '"remote_ip": "%(h)s", "method": "%(m)s", "path": "%(U)s", '
+    '"query": "%(q)s", "status": %(s)s, "bytes": %(B)s, "time_taken": %(L)s, '
     '"user_agent": "%(a)s"}'
 )
 
-chdir = '/code'
-worker_tmp_dir = '/dev/shm'  # noqa: S108
+logconfig_dict = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'gunicorn_access': {
+            'format': '%(message)s',
+        },
+        'gunicorn_error': {
+            '()': structlog.stdlib.ProcessorFormatter,
+            'processor': structlog.processors.JSONRenderer(),
+            'foreign_pre_chain': [
+                structlog.stdlib.add_log_level,
+                structlog.stdlib.add_logger_name,
+                structlog.processors.TimeStamper(fmt='iso'),
+            ],
+        },
+    },
+    'handlers': {
+        'console_access': {
+            'class': 'logging.StreamHandler',
+            'stream': 'ext://sys.stdout',
+            'formatter': 'gunicorn_access',
+        },
+        'console_error': {
+            'class': 'logging.StreamHandler',
+            'stream': 'ext://sys.stderr',
+            'formatter': 'gunicorn_error',
+        },
+    },
+    'loggers': {
+        'gunicorn.access': {
+            'handlers': ['console_access'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+        'gunicorn.error': {
+            'handlers': ['console_error'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+    },
+    'root': {
+        'level': 'INFO',
+        'handlers': ['console_error'],
+    },
+}
